8c2dbfd461b98c1b78041b8ac49000498cab7b18
Summerfirefly <kayohikawa@outlook.com>
2019-06-13 22:31:30 +0800

    初步添加开始界面

---
+++ MenuSceneUI.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuSceneUI : MonoBehaviour
+{
+    public void OnStartClick()
+    {
+        SceneManager.LoadScene("PlayScene");
+    }
+
+    public void OnExitClick()
+    {
+        Application.Quit();
+    }
+}
